c7b6a930ba179d170da669ff0d40f4f22e351ca9
COLLOV <collov.j@gmail.com>
2025-10-29 17:15:00 +0100

    backend: register conversation models in models.__init__ for schema creation

--- backend/src/insight_backend/models/__init__.py
+++ backend/src/insight_backend/models/__init__.py
@@ -1,5 +1,13 @@
 from .user import User
 from .chart import Chart
 from .user_table_permission import UserTablePermission
+from .conversation import Conversation, ConversationMessage, ConversationEvent
 
-__all__ = ["User", "Chart", "UserTablePermission"]
+__all__ = [
+    "User",
+    "Chart",
+    "UserTablePermission",
+    "Conversation",
+    "ConversationMessage",
+    "ConversationEvent",
+]
